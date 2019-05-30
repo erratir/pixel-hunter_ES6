ff9bba748d44720e6fbf97d26bb6d4982605cf66
erratic <mail4entry@gmail.com>
2019-05-30 16:33:06 +0300

    module2: add module utils.js & function createDomElement

---
+++ js/utils.js
@@ -0,0 +1,11 @@
+/**
+ * Создает DOM-элемент на основе переданной в виде строки разметки (html кода).
+ * Функция принимает на вход строку с разметкой и возвращать DOM-элемент.
+ * @param {string} html
+ * @return {HTMLElement}
+ */
+export const createDomElement = (html) => {
+  const template = document.createElement(`div`);
+  template.innerHTML = html.trim(); // trim() - удалить пробелы вначале и конце строки
+  return template;
+};
