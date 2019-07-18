f6ef524539a3b4d2075913551e9c42421652c48d
erratir <mail4entry@gmail.com>
2019-07-19 01:49:18 +0300

    module3: code refactoring

--- js/data/statistic.js
+++ js/data/statistic.js
@@ -2,73 +2,77 @@
  * object settings rules of the game
  * @type {{levels: number, gameTime: number, warningTime: number, slowTime: number, quickTime: number, lives: number, liveBonus: number, answersPoints: {wrong: number, correct: number, fast: number, slow: number}}}
  */
-const RULES = {
+const RULES = Object.freeze({
   levels: 10, // количество уровней (вопросов)
   gameTime: 30,
   warningTime: 5,
-  slowTime: 20,
-  quickTime: 10,
-  lives: 3, // Жизней (возможно вариантов не правильных ответов)
+  slowTime: 20, // slow answer time
+  quickTime: 10, // fast answer time
+  lives: 3, // Кол-во жизней (кол-во возможных ошибок)
   liveBonus: 50, // Бонус за каждую сохраненную жизнь
-  answersPoints: {
-    wrong: 0,
-    correct: 100, // 10-20 секунд
-    fast: 150, // < 10 сек
-    slow: 50 // > 20 сек
+  answersPoints: { // к-во очков за ответы:
+    wrong: 0, // за неверный ответ
+    correct: 100, // за верный ответ произведенный за Хсек (quickTime >  Хсек < slowTime)
+    fast: 150, // за верный ответ быстрый ответ ( < quickTime сек)
+    slow: 50 // за верный медленный  ответ ( > slowTime сек)
   }
-};
+});
+
+/**
+ * initial game settings
+ */
+const INITIAL_GAME = Object.freeze({
+  correctAnswersCount: 0,
+  fastAnswersCount: 0,
+  slowAnswersCount: 0,
+  livesBonus: 0, // Бонус за сохраненные жизни
+  totalResult: {
+    success: false,
+    score: 0,
+  }
+});
 
 /**
  * Функция подсчёта очков при окончании игры / Scoring function at the end of the game
  * @param {number} lives - Количество оставшихся жизней
  * @param {array} answers - Массив ответов хранит данные об ответах пользователя на каждый вопрос по порядку.
  *                          варианты ответов: см - RULES.answersPoints
- * @return {number}
+ * @return {object} newGame - объект со статистикой игры
  */
 export const calculateStatistic = ({lives, answers}) => {
 
-  /**
-   * initial game settings
-   */
-  const STAT = {
-    correctAnswersCount: 0,
-    fastAnswersCount: 0,
-    slowAnswersCount: 0,
-    livesBonus: 0, // Бонус за сохраненные жизни
-    totalResult: {
-      success: false,
-      score: 0,
-    }
-  };
+  // скопируем объект INITIAL_GAME
+  const newGame = Object.assign({}, INITIAL_GAME);
 
   /**
    *   Если игрок ответил меньше, чем на 10 вопросов, то игра считается не пройденной и функция должна вернуть -1;
    *   Жизней может быть 0-3 type@ integer
    */
   if (!Number.isInteger(lives) || !Array.isArray(answers) || answers.length !== RULES.levels || lives < 0 || lives > 3) {
-    return -1;
+    newGame.totalResult.score = -1;
+    return newGame;
   } else {
-    STAT.totalResult.success = true;
+    newGame.totalResult.success = true;
   }
 
   // подсчет кол-ва различных ответов
   answers.forEach((item) => {
     if (item === `correct`) {
-      STAT.correctAnswersCount += 1;
+      newGame.correctAnswersCount += 1;
     } else if (item === `fast`) {
-      STAT.fastAnswersCount += 1;
+      newGame.fastAnswersCount += 1;
     } else if (item === `slow`) {
-      STAT.slowAnswersCount += 1;
+      newGame.slowAnswersCount += 1;
     }
   });
 
-  STAT.livesBonus = lives * RULES.liveBonus;
+  newGame.livesBonus = lives * RULES.liveBonus;
 
-  STAT.totalResult.score =
-    STAT.correctAnswersCount * RULES.answersPoints.correct +
-    STAT.fastAnswersCount * RULES.answersPoints.fast +
-    STAT.slowAnswersCount * RULES.answersPoints.slow +
-    STAT.livesBonus;
+  newGame.totalResult.score =
+    newGame.correctAnswersCount * RULES.answersPoints.correct +
+    newGame.fastAnswersCount * RULES.answersPoints.fast +
+    newGame.slowAnswersCount * RULES.answersPoints.slow +
+    newGame.livesBonus;
 
-  return STAT.totalResult.score;
+  return newGame;
 };
--- js/data/statistic.test.js
+++ js/data/statistic.test.js
@@ -8,45 +8,47 @@ let answers = [];
 
 describe(`-=== Game Statistic ===-`, () => {
   describe(`If the player answers less than 10 questions, the game is considered not passed and the function should return -1;`, () => {
-    it(`should return -1 if answers.length <> 10.`, () => {
+    it(`should return -1 if answers.length != 10.`, () => {
       answers = [`correct`, `wrong`];
-      assert.equal(calculateStatistic({lives: 3, answers}), -1);
+      assert.equal(calculateStatistic({lives: 3, answers}).totalResult.score, -1);
       answers = [];
-      assert.equal(calculateStatistic({lives: 3, answers}), -1);
+      assert.equal(calculateStatistic({lives: 3, answers}).totalResult.score, -1);
       answers = [`correct`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `wrong`];
-      assert.equal(calculateStatistic({lives: 3, answers}), -1);
+      assert.equal(calculateStatistic({lives: 3, answers}).totalResult.score, -1);
     });
   });
   describe(`If player answered all questions and not fast and not slow, and he had all (3) lives, function should return 1150 points`, () => {
     it(`should return 1150 points`, () => {
       answers = [`correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`];
-      assert.equal(calculateStatistic({lives: 3, answers}), 1150);
+      assert.equal(calculateStatistic({lives: 3, answers}).totalResult.score, 1150);
     });
     it(`should return 1050 points`, () => {
-      assert.equal(calculateStatistic({lives: 1, answers}), 1050);
+      assert.equal(calculateStatistic({lives: 1, answers}).totalResult.score, 1050);
     });
     it(`should return 1000 points`, () => {
-      assert.equal(calculateStatistic({lives: 0, answers}), 1000);
+      assert.equal(calculateStatistic({lives: 0, answers}).totalResult.score, 1000);
     });
     it(`should return 1150 points`, () => {
       answers = [`fast`, `correct`, `correct`, `correct`, `wrong`, `correct`, `correct`, `correct`, `slow`, `correct`];
-      assert.equal(calculateStatistic({lives: 3, answers}), 1050);
+      assert.equal(calculateStatistic({lives: 3, answers}).totalResult.score, 1050);
     });
     it(`should return 250 points`, () => {
       answers = [`wrong`, `wrong`, `wrong`, `wrong`, `wrong`, `slow`, `slow`, `slow`, `slow`, `slow`];
-      assert.equal(calculateStatistic({lives: 0, answers}), 250);
+      assert.equal(calculateStatistic({lives: 0, answers}).totalResult.score, 250);
     });
   });
   describe(`invalid data`, () => {
     it(`should return -1`, () => {
       answers = [`correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `correct`];
-      assert.equal(calculateStatistic({lives: 22, answers}), -1);
+      assert.equal(calculateStatistic({lives: 22, answers}).totalResult.score, -1);
+      assert.equal(calculateStatistic({lives: -999, answers}).totalResult.score, -1);
+      assert.equal(calculateStatistic({lives: `string`, answers}).totalResult.score, -1);
+      assert.equal(calculateStatistic({lives: true, answers}).totalResult.score, -1);
       answers = `0123456789`;
-      assert.equal(calculateStatistic({lives: {3: 2}, answers}), -1);
+      assert.equal(calculateStatistic({lives: [3, 2], answers}).totalResult.score, -1);
       answers = `qwertyuiop`;
-      assert.equal(calculateStatistic({lives: 3, answers}), -1);
-      assert.equal(calculateStatistic({lives: true, answers}), -1);
-      assert.equal(calculateStatistic(1), -1);
+      assert.equal(calculateStatistic({lives: 3, answers}).totalResult.score, -1);
+      assert.equal(calculateStatistic(1).totalResult.score, -1);
     });
   });
 });
