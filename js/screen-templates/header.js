95d4adbf95a7a990a2e1b489259685d10fe904e7
erratic <mail4entry@gmail.com>
2019-08-26 19:15:23 +0300

    module4: headerTemplate now this is a function, that gets the game state as an argument

--- js/screen-templates/header.js
+++ js/screen-templates/header.js
@@ -1,18 +1,25 @@
 import {buttonBackHtml} from "./button-back-html";
-import {INITIAL_STATE} from "../data/data";
 
 const emptyHeartHtml = `<img src="img/heart__empty.svg" class="game__heart" alt=" Missed Life" width="31" height="27">`;
 const fullHeartHtml = `<img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">`;
 
 
 // [].fill().join(``) заполнить все элементы массива, объеденить в строку
-const headerTemplate = `<header class="header">
+
+/**
+ * Принимает на вход объект описывающий состояние игры, возвращает строку с html кодом для отрисовки header
+ * @param {object} state
+ * @return {string}
+ */
+const headerTemplate = (state) => {
+  return `<header class="header">
   ${buttonBackHtml}
-  <div class="game__timer">${INITIAL_STATE.time}</div>
+  <div class="game__timer">${state.time}</div>
   <div class="game__lives">
-  ${new Array(3 - INITIAL_STATE.lives).fill(emptyHeartHtml).join(``)}
-  ${new Array(INITIAL_STATE.lives).fill(fullHeartHtml).join(``)}
+  ${new Array(3 - state.lives).fill(emptyHeartHtml).join(``)}
+  ${new Array(state.lives).fill(fullHeartHtml).join(``)}
   </div>
   </header>`;
+};
 
 export {headerTemplate};
